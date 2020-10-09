6c11b1665925580c629c5b565e74cb9b02443542
JJack27 <yizhouzhao27@gmail.com>
2020-10-08 21:32:07 -0600

    add scan

--- gattServer.py
+++ gattServer.py
@@ -1,15 +1,21 @@
 import pygatt
 
-adapter = pygatt.GATTToolBackend()
+def connectDevice(macAddr):
+    adapter = pygatt.GATTToolBackend()
+    try:
+        adapter.start()
+        device = adapter.connect('5C:17:CF:0E:D0:74')
+        
+        print("device connected")
+        while True:
+            # should receiving data
+            pass
+    except:
+        adapter.stop()
+    finally:
+        adapter.stop()
 
-try:
-    adapter.start()
-    device = adapter.connect('5C:17:CF:0E:D0:74')
-    
-    print("device connected")
-    while True:
-        pass
-except:
-    adapter.stop()
-finally:
-    adapter.stop()
+def main():
+    # find all devices that are matching with mac address pattern
+    adapterScan = pygatt.GATTToolBackend()
+    print(adapterScan.scan)
