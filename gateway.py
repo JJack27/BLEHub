82500e5c5a9deb6418c3340c50fbe6429e619981
Jack <yizhouzhao27@gmail.com>
2020-10-10 13:55:09 -0600

    add gateway

--- gateway.py
+++ gateway.py
@@ -1,2 +1,21 @@
 import pygatt
 
+'''
+Gateway that responsible for:
+    - Discovering nearby bracelets based on MAC address
+    - Raise subprocesses when NEW bracelet is detected
+    - Maintain the list of MAC addresses
+    - Keep tracking MAC address and raised subprocesses
+'''
+class Gateway:
+
+    def __init__(self):
+        # self._mac_addr = []
+        
+        # maps mac address to pid of sub-process
+        self._mac_proc_table = {}
+
+    def update_mac_table(self, mac_addrs):
+        pass
+
+     
